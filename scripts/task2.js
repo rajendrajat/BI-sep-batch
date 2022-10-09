b47acd6c63aec2687ae77cf1a7814fd03467633d
rajendrajat <rajendrajat022@gmail.com>
2022-10-09 16:30:07 +0530

    v1.0

---
+++ scripts/task2.js
@@ -0,0 +1,23 @@
+/*let year = prompt('In which year was the ECMAScript 2015 specification published?', '');
+
+if (year < 2015) {
+    alert('Too early...');
+} else if (year > 2015) {
+    alert('Too late');
+} else {
+    alert('Exactly!');
+}
+*/
+
+
+let year = prompt('When is Rajendra birthday year', '');
+
+if (year < 1996) {
+    alert('ohhh nooo')
+} else if (year > 1996) {
+    alert('you forget my birthday')
+} else {
+    alert('Thankyou you are my real friend', '');
+}
+
+
